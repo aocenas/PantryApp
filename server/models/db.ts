8fa4a24bfb652a909268b3145761205f879b1735
Andrej Ocenas <mr.ocenas@gmail.com>
2016-07-12 22:09:01 +0200

    comments

--- server/models/db.ts
+++ server/models/db.ts
@@ -3,13 +3,16 @@ const Sequelize = require('sequelize');
 // SQLite, by default in memory, should be enough for the demo
 export default new Sequelize({
     dialect: 'sqlite',
+
+    // TODO: logging
+    // logging: false,
+
     define: {
         paranoid: true,
 
-        // probably not needed here as I use sqlite, but in case of using posgress I would make sure everything is
+        // probably not needed here as we use sqlite, but in case of using posgress I would make sure everything is
         // properly underscored.
         // underscored: true,
         // underscoredAll: true,
     },
 });
-
